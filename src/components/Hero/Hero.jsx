758240fb2b9f447461145da311a190aabcf2ef95
Nikhil Shinde <nikhil.shinde@vshsolutions.com>
2021-05-24 23:46:28 +0530

    Image updates & emojis added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -29,7 +29,7 @@ const Header = () => {
             {title || 'Hi, my name is'}{' '}
             <span className="text-color-main">{name || 'Nikhil Shinde'}</span>
             <br />
-            {subtitle || "I'm an Software Engineer."}
+            {subtitle || "I'm an Software Engineer 💻."}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
